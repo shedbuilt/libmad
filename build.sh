00bf5c78aeb67241ea16ff796e66041ccd8f5eb5
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:54:20 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-patch -Np1 -i "${SHED_PATCHDIR}/libmad-0.15.1b-fixes-1.patch" && \
+patch -Np1 -i "${SHED_PKG_PATCH_DIR}/libmad-0.15.1b-fixes-1.patch" && \
 sed "s@AM_CONFIG_HEADER@AC_CONFIG_HEADERS@g" -i configure.ac && \
 touch NEWS AUTHORS ChangeLog && \
 autoreconf -fiv && \
 ./configure --prefix=/usr \
             --disable-static && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install && \
-install -Dm644 -v "${SHED_CONTRIBDIR}/mad.pc" "${SHED_FAKEROOT}/usr/lib/pkgconfig/mad.pc"
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install && \
+install -Dm644 -v "${SHED_PKG_CONTRIB_DIR}/mad.pc" "${SHED_FAKE_ROOT}/usr/lib/pkgconfig/mad.pc"
